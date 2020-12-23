4fefa23eaa0b156d582400c1bc9c8156b7fe423a
Shreyas <shreyaskowshik@gmail.com>
2020-12-24 00:00:11 +0530

    Add ArgParse code

--- example_train.jl
+++ example_train.jl
@@ -6,6 +6,7 @@ using LogicCircuits
 using ProbabilisticCircuits
 using ILStrudel
 using Statistics
+using ArgParse
 
 function single_model()
     # pc = learn_single_model("nltcs")
@@ -22,20 +23,20 @@ function single_model()
         return_vtree=false)
 end
 
-function mine_model()
-    train_x, valid_x, test_x = twenty_datasets("plants")
+function mine_model(dataset_name)
+    train_x, valid_x, test_x = twenty_datasets(dataset_name)
     pick_edge = "eFlow"
     pick_var = "vMI"
-    population_size=100
+    population_size=1000
     
     pcs, bitmasks = learn_mine_ensemble(train_x, valid_x, test_x;
         mine_iterations=1,
         population_size=population_size,
-        num_mine_samples=5,
-        pick_edge=pick_edge, pick_var=pick_var, depth=1, 
+        num_mine_samples=10,
+        pick_edge=pick_edge, pick_var=pick_var, depth=1,
         pseudocount=1e-9,
         sanity_check=true,
-        maxiter=200,
+        maxiter=350,
         seed=nothing,
         return_vtree=false,
         return_bitmasks=true)
@@ -87,4 +88,44 @@ function mine_model()
     # println(test_ll)
 end
 
-mine_model()
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--name"
+            help = "Name of the dataset"
+            arg_type = String
+      		required = true
+
+        # "--split_h"
+        #     help = "Split Heuristic"
+        #     arg_type = String
+        #     required = true
+
+        # "--clone_h"
+        #     help = "Clone Heuristic"
+        #     arg_type = String
+        #     required = true
+
+        # "--merge_h"
+        #     help = "Merge Heuristic"
+        #     arg_type = String
+        #     required = true
+        # "--opt2", "-o"
+        #     help = "another option with an argument"
+        #     arg_type = Int
+        #     default = 0
+        # "--flag1"
+        #     help = "an option without argument, i.e. a flag"
+        #     action = :store_true
+        # "arg1"
+        #     help = "a positional argument"
+        #     required = true
+    end
+
+    return parse_args(s)
+end
+
+parsed_args = parse_commandline()
+
+mine_model(parsed_args["name"])
