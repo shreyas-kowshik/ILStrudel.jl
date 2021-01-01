edc7f63b42ced5df57ed7070a0e827fc377d6145
shreyas-kowshik <shreyaskowshik@gmail.com>
2021-01-01 06:24:17 -0800

    Minor Update

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ datasets = datasets[min_len:max_len]
 
 for (i,dataset) in enumerate(datasets):
     sess = dataset
-    command = "tmux new-session -d -s {} 'julia example_train.jl --name {} && julia'".format(sess, dataset)
+    command = "tmux new-session -d -s {} 'julia example_train.jl --name {} --population_size 100 --pmi_thresh 0.01 --pseudocount 1.0 --maxiter 100 --num_mine_samples 15 --mine_iterations 3'".format(sess, dataset)
     args = shlex.split(command)
     subprocess.Popen(args)
 
