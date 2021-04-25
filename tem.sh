a2a57e28dc7508901e7c9b17ca7b20ce6919211b
shreyas-kowshik <shreyaskowshik@gmail.com>
2021-04-24 23:57:12 -0700

    CHECKPOINT : So far the vtrees are learned on each bitmask

---
+++ tem.sh
@@ -0,0 +1,11 @@
+tmux kill-session -t accidents
+tmux kill-session -t ad
+tmux kill-session -t bbc
+tmux kill-session -t baudio
+tmux kill-session -t bnetflix
+tmux kill-session -t cr52
+tmux kill-session -t c20ng
+tmux kill-session -t dna
+tmux kill-session -t book
+tmux kill-session -t cwebkb
+
--- train.py
+++ train.py
@@ -11,11 +11,13 @@ min_len = 0
 max_len = 20
 datasets = datasets[min_len:max_len]
 # datasets = datasets[max_len:]
+datasets = ['c20ng']
 
 for (i,dataset) in enumerate(datasets):
     sess = dataset
-    run_name = "mine_em_bagging_size_thresh_1"
-    command = "tmux new-session -d -s {} 'julia1 example_train.jl --name {} --run_name {} --pseudocount 1.0 --maxiter 1000 --pmi_thresh 0.03 --population_size 300 --num_mine_samples 10 --mine_iterations 3 --num_mi_bags 20 --bitmask_path ~/runs/mine_em_bagging_size_thresh_1/{}/bitmasks.jld'".format(sess, dataset, run_name, dataset)
+    run_name = "mine_em_bagging_size_thresh_2"
+    # command = "tmux new-session -d -s {} 'julia1 example_train.jl --name {} --run_name {} --pseudocount 1.0 --maxiter 150 --pmi_thresh 0.03 --population_size 300 --num_mine_samples 5 --mine_iterations 3 --num_mi_bags 50'".format(sess, dataset, run_name)
+    command = "tmux new-session -d -s {} 'julia1 example_train.jl --name {} --run_name {} --pseudocount 1.0 --maxiter 1000 --pmi_thresh 0.03 --population_size 300 --num_mine_samples 5 --mine_iterations 3 --num_mi_bags 50 --bitmask_path ~/runs/mine_em_bagging_size_thresh_2/{}/bitmasks.jld'".format(sess, dataset, run_name, dataset)
     args = shlex.split(command)
     subprocess.Popen(args)
 
