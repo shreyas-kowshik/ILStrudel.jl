c56ba3316cf691009bc1998aa2855f36403165a3
shreyas-kowshik <shreyaskowshik@gmail.com>
2021-05-06 06:21:47 -0700

    Minor Changes

--- tem.sh
+++ tem.sh
@@ -4,6 +4,7 @@ tmux kill-session -t bbc
 tmux kill-session -t baudio
 tmux kill-session -t bnetflix
 tmux kill-session -t cr52
+tmux kill-session -t c20ng
 tmux kill-session -t dna
 tmux kill-session -t book
 tmux kill-session -t cwebkb
